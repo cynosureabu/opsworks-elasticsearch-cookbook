35f8601d2be52510419eb60f6ddc6d65b8374f21
Chen Wang <cynosureabu@gmail.com>
2015-12-22 08:11:26 -0800

    Update esplugins.rb

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -4,7 +4,6 @@ script "install_plugin_es_head" do
 	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
 	code <<-EOH
   	plugin -install mobz/elasticsearch-head
-  	plugin -install cloud-aws
   	EOH
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
